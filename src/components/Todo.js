f368ba2fbf11ea945425cce58c32281106528380
scokljat <90629541+scokljat@users.noreply.github.com>
2021-11-26 23:13:28 +0100

    Adds new components

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,12 +3,15 @@ import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 
 function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
   });
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const submitUpdate = (value) => {
     updateTodo(edit.id, value);
     setEdit({
@@ -16,27 +19,41 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
       value: "",
     });
   };
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
   if (edit.id) {
     return <TodoForm edit={edit} onSubmit={submitUpdate} />;
   }
 
-  return todos.map((todo, index) => (
-    <div className="todoRow" key={index}>
-      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-        {todo.text}
-      </div>
-      <div className="icons">
-        <RiCloseCircleLine
-          onClick={() => removeTodo(todo.id)}
-          className="deleteIcon"
-        />
-        <TiEdit
-          onClick={() => setEdit({ id: todo.id, value: todo.text })}
-          className="editIcon"
-        />
-      </div>
-    </div>
-  ));
+  return (
+    <>
+      {todos.map((todo, index) => (
+        <div className="todoRow" key={index}>
+          <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+            {todo.text}
+          </div>
+          <div className="icons">
+            <RiCloseCircleLine
+              onClick={() => {
+                setModalIsOpen(true);
+              }}
+              className="deleteIcon"
+            />
+            <TiEdit
+              onClick={() => setEdit({ id: todo.id, value: todo.text })}
+              className="editIcon"
+            />
+          </div>
+        </div>
+      ))}
+      ;
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={removeTodo} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
+    </>
+  );
 }
 
 export default Todo;
